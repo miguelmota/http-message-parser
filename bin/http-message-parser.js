77fc44c71291d29c63bcdc426f825f9bb1d4faa3
Miguel Mota <miguelmota2@gmail.com>
2016-02-25 01:28:14 -0800

    debug

--- bin/http-message-parser.js
+++ bin/http-message-parser.js
@@ -1,57 +1,6 @@
 #!/usr/bin/env node
 
+'use strict';
 var path = require('path');
-var fs = require('fs');
-var httpMessageParser = require(path.join('../http-message-parser'));
-var concat = require('concat-stream');
-var streamBuffers = require('stream-buffers');
-var getProp = require('get-prop');
+require(path.join('../lib/ci'));
 
-var argv = require('minimist')(process.argv.slice(2));
-
-if (argv._[0]) {
-  fs.createReadStream(path.resolve(argv._[0])).pipe(concat(gotData));
-} else {
-  process.stdin.pipe(concat(gotData));
-}
-
-function gotData(buffer) {
-  var parsed = httpMessageParser(buffer);
-  var result = parsed;
-
-  if (result instanceof Object) {
-    if (argv.only) {
-      result = getProp(parsed, argv.only);
-    }
-
-    if (result instanceof Object && Buffer.isBuffer(result.body)) {
-      result = bodyToString(result);
-    }
-
-    if (Array.isArray(result)) {
-      result = result.map(bodyToString);
-    }
-
-    if (result instanceof Object && Array.isArray(result.multipart)) {
-      result.multipart = result.multipart.map(bodyToString);
-    }
-
-    if (!Buffer.isBuffer(result) && result instanceof Object) {
-      result = JSON.stringify(result);
-    }
-  }
-
-  var outStream = new streamBuffers.ReadableStreamBuffer();
-  outStream.put(new Buffer(result||''));
-  outStream.stop();
-
-  outStream.pipe(process.stdout);
-}
-
-function bodyToString(obj) {
-  if (Buffer.isBuffer(obj.body)) {
-    obj.body = obj.body.toString();
-  }
-
-  return obj;
-}
